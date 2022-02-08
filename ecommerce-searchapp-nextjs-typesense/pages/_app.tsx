a3b4463e3c563efe15850a45a6399a3bf54b0b59
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-08 13:10:06 +0545

    tailwind config update

--- ecommerce-searchapp-nextjs-typesense/pages/_app.tsx
+++ ecommerce-searchapp-nextjs-typesense/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
--- ecommerce-searchapp-nextjs-typesense/tailwind.config.js
+++ ecommerce-searchapp-nextjs-typesense/tailwind.config.js
@@ -3,11 +3,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [],
 };
