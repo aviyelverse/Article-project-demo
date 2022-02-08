009d37096a032984cfb3b5dc1b863de02e68cf9b
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-08 13:58:27 +0545

    Light and dark mode

--- ecommerce-searchapp-nextjs-typesense/tailwind.config.js
+++ ecommerce-searchapp-nextjs-typesense/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 200ms ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
