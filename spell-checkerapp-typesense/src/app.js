c39ad3e46985c73424904db1cbf80ee2dc2684b6
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-11 00:06:15 +0545

    spellchecker app project initialized

---
+++ spell-checkerapp-typesense/src/app.js
@@ -0,0 +1,37 @@
+const { algoliasearch, instantsearch } = window;
+
+const searchClient = algoliasearch('latency', 'spellcheck');
+
+const search = instantsearch({
+  indexName: 'words',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+  }),
+  instantsearch.widgets.configure({
+    facets: ['*'],
+    maxValuesPerFacet: 20,
+  }),
+  instantsearch.widgets.dynamicWidgets({
+    container: '#dynamic-widgets',
+    fallbackWidget({ container, attribute }) {
+      return instantsearch.widgets.refinementList({
+        container,
+        attribute,
+      });
+    },
+    widgets: [
+    ],
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();
