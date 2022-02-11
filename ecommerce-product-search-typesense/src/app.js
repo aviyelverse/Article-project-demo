e92a29003abec5066fe66ea7b7985b43b5aa58a9
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-11 18:00:37 +0545

    ecommerce product search app initialized

---
+++ ecommerce-product-search-typesense/src/app.js
@@ -0,0 +1,36 @@
+const { algoliasearch, instantsearch } = window;
+
+const searchClient = algoliasearch('latency', 'ecommerceapp');
+
+const search = instantsearch({
+  indexName: 'products',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+  }),
+  instantsearch.widgets.configure({
+    facets: ['*'],
+    maxValuesPerFacet: 20,
+  }),
+  instantsearch.widgets.dynamicWidgets({
+    container: '#dynamic-widgets',
+    fallbackWidget({ container, attribute }) {
+      return instantsearch.widgets.refinementList({
+        container,
+        attribute,
+      });
+    },
+    widgets: [],
+  }),
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  }),
+]);
+
+search.start();
