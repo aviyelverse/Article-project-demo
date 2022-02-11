6cfcc52d57a6a7cd326ce559970f5ab81e6d7f8d
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-11 19:21:04 +0545

    All issue fix

--- ecommerce-product-search-typesense/src/app.js
+++ ecommerce-product-search-typesense/src/app.js
@@ -26,11 +26,41 @@ search.addWidgets([
   instantsearch.widgets.searchBox({
     container: '#searchbox',
   }),
+  instantsearch.widgets.configure({
+    hitsPerPage: 4,
+  }),
   instantsearch.widgets.hits({
     container: '#hits',
+    templates: {
+      item: `
+        <div>
+          <img src="{{image}}" align="left" alt="" />
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-description">
+            {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-price">{{ price }}</div>
+          <div class="hit-rating">Brand: {{brand}}</div>
+        </div>
+      `,
+    },
   }),
   instantsearch.widgets.pagination({
     container: '#pagination',
+  }),
+  instantsearch.widgets.refinementList({
+    container: '#filterList',
+    attribute: 'categories',
+  }),
+  instantsearch.widgets.sortBy({
+    container: '#sort',
+    items: [
+      { label: 'Default', value: 'products' },
+      { label: 'Price (asc)', value: 'products/sort/price:asc' },
+      { label: 'Price (desc)', value: 'products/sort/price:desc' },
+    ],
   }),
 ]);
 
