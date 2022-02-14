2dfed0aae9a316f1cadc5463eaaf3f8ca1bb1fd0
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-14 17:52:56 +0545

    Fix run server script

--- anime-searchapp-reactjs-typesense/scripts/runServer.js
+++ anime-searchapp-reactjs-typesense/scripts/runServer.js
@@ -4,7 +4,7 @@ const command = `docker run -d -p 8108:8108 -v\`pwd\`/typesense-server-data/:/da
 typesense/typesense:0.22.2 --data-dir /data --api-key=animesearch --listen-port 8108 --enable-cors`;
 
 exec(command, (err) => {
-  if (!err) console.log("Typesense Server is running...");
+  if (!err) console.log("Typesense Server is up and running...✰✨");
 
   if (err) {
     console.log("Error running server: ", err);
