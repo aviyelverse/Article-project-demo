7e54b0bd376efd75b19aff0fe2ca981848663632
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-19 13:38:11 +0545

    Update Nextjs chatwoot Integration

---
+++ nextjs-chatwoot-integration/pages/api/posts.js
@@ -0,0 +1,43 @@
+export default (req, res) => {
+  res.status(200).json({
+    posts: [
+      // 100 fake post
+      {
+        id: 1,
+        title: "Learn Nextjs from scratch [Part-1]",
+        body: "A very comprehensive and thorough guide about Nextjs",
+        published: true,
+      },
+      {
+        id: 2,
+        title: "Learn Nextjs from scratch [Part-2]",
+        body: "A very comprehensive and thorough guide about Nextjs",
+        published: true,
+      },
+      {
+        id: 3,
+        title: "Learn Nextjs from scratch [Part-3]",
+        body: "A very comprehensive and thorough guide about Nextjs",
+        published: true,
+      },
+      {
+        id: 4,
+        title: "Learn Nextjs from scratch [Part-4]",
+        body: "A very comprehensive and thorough guide about Nextjs",
+        published: true,
+      },
+      {
+        id: 5,
+        title: "Learn Nextjs from scratch [Part-5]",
+        body: "A very comprehensive and thorough guide about Nextjs",
+        published: true,
+      },
+      {
+        id: 6,
+        title: "Learn Nextjs from scratch [Part-6]",
+        body: "A very comprehensive and thorough guide about Nextjs",
+        published: true,
+      },
+    ],
+  });
+};
