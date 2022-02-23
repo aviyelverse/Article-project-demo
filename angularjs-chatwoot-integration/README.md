bf83ba6493e310bf95e01a93a3ddaac6c52aa92c
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-23 21:59:11 +0545

    Update project

--- angularjs-chatwoot-integration/README.md
+++ angularjs-chatwoot-integration/README.md
@@ -1,4 +1,4 @@
-# AngularChatwootIntegration
+# AngularjsChatwootIntegration
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.4.
 
--- angularjs-chatwoot-integration/src/app/app.component.spec.ts
+++ angularjs-chatwoot-integration/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-chatwoot-integration'`, () => {
+  it(`should have as title 'angularjs-chatwoot-integration'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-chatwoot-integration');
+    expect(app.title).toEqual('angularjs-chatwoot-integration');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-chatwoot-integration app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularjs-chatwoot-integration app is running!');
   });
 });
