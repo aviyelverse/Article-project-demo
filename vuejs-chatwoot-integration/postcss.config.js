59cccfb30227af431cb6c05866b8a9379cd9fb8b
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-23 14:11:50 +0545

    Add tailwind

---
+++ vuejs-chatwoot-integration/postcss.config.js
@@ -0,0 +1,8 @@
+// postcss config for vue
+
+const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
