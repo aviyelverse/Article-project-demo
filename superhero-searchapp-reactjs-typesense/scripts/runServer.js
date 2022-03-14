be6fe962c7e64ca1841141005a0df4905da82d17
pramit-marattha <pramitmarattha5497@gmail.com>
2022-03-14 12:12:30 +0545

    Project cleanup and fix

---
+++ superhero-searchapp-reactjs-typesense/scripts/runServer.js
@@ -0,0 +1,10 @@
+const { exec } = require("child_process");
+const command = `docker run -d -p 8108:8108 -v/tmp/typesense-server-data/:/data \ typesense/typesense:0.22.2 --data-dir /data --api-key=superhero --listen-port 8108 --enable-cors`;
+
+exec(command, (err) => {
+  if (!err) console.log("Typesense Server is up and running...✰✨");
+
+  if (err) {
+    console.log("Error running server: ", err);
+  }
+});
